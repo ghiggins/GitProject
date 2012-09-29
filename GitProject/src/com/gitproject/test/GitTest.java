0d3c6f945a8f2d81cf0c3c7973faae666fdd5927
Gabriel <ghiggins21@hotmail.com>
2012-09-29 15:18:09 +0100

    Second Commit

--- GitProject/src/com/gitproject/test/GitTest.java
+++ GitProject/src/com/gitproject/test/GitTest.java
@@ -2,13 +2,51 @@ package com.gitproject.test;
 
 public class GitTest {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-
+		
+		GitTest gt = new GitTest();
+		gt.go();
 	}
-
+	
+	public void go(){
+	
+		System.out.print("TTTTTTTTTTTTTT RRRRRRRRRRRRRRRR\t              AAAAAA\t        DDDDDDDDDDD\t      IIIIIIIIIII    NNNN\t   NNNN\t   GGGGGGGG");
+		System.out.println();
+		System.out.print("TTTTTTTTTTTTTT   RRRRRRRRRRRRRRRRR              AAAAAAAA         DDDDDDDDDDDDD      IIIIIIIIIII    NNNNN    NNNN    GGGGGGGGGG");
+		System.out.println();
+		System.out.print("    TTTTT        RRRRR      RRRRRRR            AAAAAAAAAA        DDDD      DDDD        IIIII       NNNNNN   NNNN   GGGG        ");
+		System.out.println();
+		System.out.print("    TTTTT        RRRRR      RRRRRRR           AAAA    AAAA       DDDD      DDDDD       IIIII       NNNNNNN  NNNN   GGGG        ");
+		System.out.println();
+		System.out.print("    TTTTT        RRRRRRRRRRRRRRRR            AAAAA    AAAAA      DDDD      DDDDD       IIIII       NNNN  NNNNNNN   GGGG    GGGG");
+		System.out.println();
+		System.out.print("    TTTTT        RRRRRRRRRRRRRRRR           AAAAAAAAAAAAAAAA     DDDD      DDDDD       IIIII       NNNN   NNNNNN   GGGG     GGG");
+		System.out.println();
+		System.out.print("    TTTTT        RRRRR      RRRRRR         AAAAAAAAAAAAAAAAAA    DDDD      DDDD        IIIII       NNNN    NNNNN   GGGG     GGG");
+		System.out.println();
+		System.out.print("    TTTTT        RRRRR        RRRRR       AAAAA          AAAAA   DDDDDDDDDDDDD      IIIIIIIIIII    NNNN     NNNN   GGGGGGGGGGGG");
+		System.out.println();
+		System.out.print("    TTTTT        RRRRR         RRRRR     AAAAA            AAAAA  DDDDDDDDDDDD       IIIIIIIIIII    NNNN      NNN    GGGGGGGGGGG");
+		System.out.println();
+		System.out.println();
+		System.out.print("PPPPPPPPPPPP     LLLLL                      AAAAA         TTTTTTTTTTTTTT    FFFFFFFFFFFF     OOOOOOOO     RRRRRRRRRRRRRR       MMM          MMM ");
+		System.out.println();
+		System.out.print("PPPPPPPPPPPPP    LLLLL                     AAAAAAA        TTTTTTTTTTTTTT    FFFFFFFFFFFF    OOOOOOOOOO    RRRRRRRRRRRRRRR      MMMM        MMMM");
+		System.out.println();
+		System.out.print("PPP       PPPP   LLLLL                    AAAAAAAAA           TTTTT         FFFF            OOO    OOO    RRRRR     RRRRRR     MMMMM      MMMMM ");
+		System.out.println();
+		System.out.print("PPP       PPPP   LLLLL                   AAAA   AAAA          TTTTT         FFFF            OOO    OOO    RRRRR     RRRRRR     MMMMMM    MMMMMM");
+		System.out.println();
+		System.out.print("PPP       PPPP   LLLLL                  AAAA     AAAA         TTTTT         FFFFFFFFFFF     OOO    OOO    RRRRR     RRRRR      MMMMMMMMMMMMMMMM");
+		System.out.println();
+		System.out.print("PPPPPPPPPPPPP    LLLLL                 AAAAA     AAAAA        TTTTT         FFFFFFFFFFF     OOO    OOO    RRRRRRRRRRRRRR       MMMMMMMMMMMMMMMM");
+		System.out.println();
+		System.out.print("PPPPPPPPPPP      LLLLL                AAAAAAAAAAAAAAAAA       TTTTT         FFFF            OOO    OOO    RRRRRRRRRRRRRR       MMMM   MM   MMMM");
+		System.out.println();
+		System.out.print("PPPP             LLLLL               AAAAAAAAAAAAAAAAAAA      TTTTT         FFFF            OOO    OOO    RRRRR     RRRRR      MMMM        MMMM");
+		System.out.println();
+		System.out.print("PPPP             LLLLLLLLLLLLL      AAAAA           AAAAA     TTTTT         FFFF            OOOOOOOOOO    RRRRR      RRRRR     MMMM        MMMM");
+		System.out.println();
+		System.out.print("PPPP             LLLLLLLLLLLLL     AAAAA             AAAAA    TTTTT         FFFF             OOOOOOOO     RRRRR       RRRRR    MMMM        MMMM");
+		}
 }
